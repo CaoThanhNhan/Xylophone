a03d1e06f7a5814efff03a2281123e5db891abae
Cao Thành Nhân <55010539+ctnlegend@users.noreply.github.com>
2021-02-04 02:41:25 +0700

    Hoàn thành app

---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Cao Thanh Nhan based on Udemy Course by Angela Yu.
+//
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
